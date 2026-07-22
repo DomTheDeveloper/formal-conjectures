9b08804ddd17dff14660f6b0cd5d01a9636a4b23
DomTheDeveloper <DomTheDeveloper@users.noreply.github.com>
2026-07-22 16:33:57 -0700

    build(A263135): use minimal theorem imports

--- FormalConjectures/OEIS/263135.lean
+++ FormalConjectures/OEIS/263135.lean
@@ -14,7 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -/
 
-import FormalConjectures.Util.ProblemImports
+import Mathlib
+import FormalConjectures.Util.Attributes.Basic
 
 /-!
 # Maximum contacts on the honeycomb lattice
