c3cb7033f477ec38b9f910729925de848cbf7daf
DomTheDeveloper <DomTheDeveloper@users.noreply.github.com>
2026-07-22 09:08:56 -0700

    refactor(Litt): import shared definitions

--- FormalConjectures/Other/LittMostUnfairBetReversal.lean
+++ FormalConjectures/Other/LittMostUnfairBetReversal.lean
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -/
 
-import FormalConjectures.Other.LittMostUnfairBet
+import FormalConjectures.Other.LittMostUnfairBetDefs
 import Mathlib.Data.Fin.Rev
 
 /-!
