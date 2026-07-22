aef07f3099660530a57ed72a87a758e846193001
DomTheDeveloper <DomTheDeveloper@users.noreply.github.com>
2026-07-22 04:01:03 -0700

    Add temporary VC2 definition probe

---
+++ VC2Inspect.lean
@@ -0,0 +1,8 @@
+import FormalConjectures.Other.VCDimConvex
+
+#print HasAddVCNDimAtMost
+#print AddShatters
+#print HasAddVCDimAtMost
+#check Fin.sum_univ_two
+#check Matrix.cons
+#check Set.convex_iInter
