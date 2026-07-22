41bd77cf5c68fad64e00261bd041f51136cfc5f9
DomTheDeveloper <DomTheDeveloper@users.noreply.github.com>
2026-07-22 04:42:27 -0700

    remove temporary BMO 8 witness module

--- FormalConjectures/Other/BMO8Witness.lean
+++
@@ -1,9 +0,0 @@
-import FormalConjectures.Other.BeaverMathOlympiad8Proof
-
-namespace BeaverMathOlympiad
-
-@[category test, AMS 5 11 68]
-example : (1_749_056 : ℕ) = 3_498_111 / 2 + 1 := by
-  norm_num
-
-end BeaverMathOlympiad
